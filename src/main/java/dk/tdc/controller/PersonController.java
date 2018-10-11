0d0bbbe89360ec76b61b9e5e1a97fe924c14dce3
Avijit Khan <AKH@tdc.dk>
2018-10-12 00:02:54 +0200

    make it better

--- src/main/java/dk/tdc/controller/PersonController.java
+++ src/main/java/dk/tdc/controller/PersonController.java
@@ -502,7 +502,7 @@ public class PersonController {
 		    JsonNode rootNode=null;
 		    
 		    
-		    String resp="{\"fulfillmentText\": \"displayed&spoken response\",\"fulfillmentMessages\": [{\"text\": [\"messages\": [  {    \"platform\": \"facebook\",    \"speech\": \"Text response\",    \"type\": 0  }]],}],\"source\": \"example.com\",\"payload\": {\"google\": {\"expectUserResponse\": true,\"richResponse\": {\"items\": [{\"simpleResponse\": {\"textToSpeech\": \"this is a simple response\"}}]}},\"facebook\": {\"text\": \"Hello, Facebook!\"},\"slack\": {\"text\": \"This is a text response for Slack.\"}},\"outputContexts\": [{\"name\": \"projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name\",\"lifespanCount\": 5,\"parameters\": {\"param\": \"param value\"}}],\"followupEventInput\": {\"name\": \"event name\",\"languageCode\": \"en-US\",\"parameters\": {\"param\": \"param value\"}}}";
+		    String resp="{\"fulfillmentText\": \"displayed&spoken response\",\"fulfillmentMessages\": [{\"text\": {\"messages\": [  {    \"platform\": \"facebook\",    \"speech\": \"Text response\",    \"type\": 0  }]}}],\"source\": \"example.com\",\"payload\": {\"google\": {\"expectUserResponse\": true,\"richResponse\": {\"items\": [{\"simpleResponse\": {\"textToSpeech\": \"this is a simple response\"}}]}},\"facebook\": {\"text\": \"Hello, Facebook!\"},\"slack\": {\"text\": \"This is a text response for Slack.\"}},\"outputContexts\": [{\"name\": \"projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name\",\"lifespanCount\": 5,\"parameters\": {\"param\": \"param value\"}}],\"followupEventInput\": {\"name\": \"event name\",\"languageCode\": \"en-US\",\"parameters\": {\"param\": \"param value\"}}}";
 		    
 		    
 		    
